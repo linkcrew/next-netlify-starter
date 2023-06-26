c050d1fa56be4c7c0b302b2df3649a5c0920dc89
yui sakai <yu01.24mi77begining@gmail.com>
2023-06-27 00:44:10 +0900

    フッター仮

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,17 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        {/* <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} /> */}
+        <p className="mr-8">お問い合わせはSNSのDMへ</p>
+        <a href="https://www.instagram.com/yuuumi.desu/" className="mr-2">
+          <img src="/v982-d4-01_instagram.png" alt="Insragram Logo" className={styles.logo} />
+        </a>
+        <a href="https://twitter.com/kaaaaaaaazuuuu" className="mr-2">
+          <img src="/v982-d4-01_twitter.png" alt="Twitter Logo" className={styles.logo} />
+        </a>
+        <a href="https://lin.ee/2yYDBhD">
+          <img src="/v982-d4-01_line.png" alt="Line Logo" className={styles.logo} />
+        </a>
       </footer>
     </>
   )
