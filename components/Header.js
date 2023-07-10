3feb29c43cd75e7090e16c495e1231c7f4e6001e
yui sakai <yu01.24mi77begining@gmail.com>
2023-07-10 22:33:49 +0900

    ヘッダーに文章挿入

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,11 @@
 export default function Header() {
-  return <header className='w-full'>
+  return <header className='w-full relative'>
     <img
       src='/LINKCREW-header.png' layout='fill' alt='header' className="w-full"
     />
+    <div className="header-message">
+      <p>仕事の垣根を超えて</p>
+      <p>つながる大切なエンジニア仲間</p>
+    </div>
   </header>
 }
