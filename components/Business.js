d15d3d878a8e27ba546de1600c1a26fa597445bb
Hara Ryonosuke <hara_water@volbups.biz>
2023-07-30 18:24:56 +0900

    事業内容のところをレスポンシブ対応

--- components/Business.js
+++ components/Business.js
@@ -1,10 +1,10 @@
-import Card from "@mui/material/Card";
+// import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
 
 export default function Business({ title, content }) {
   return (
-    <div className="flex w-[60vw] mb-[20px]">
-      <div className="rounded-[0px] bg-card-detail-green w-1/5">
+    <div className="flex flex-col w-[60vw] mb-[20px] md:flex-row">
+      <div className="rounded-[0px] bg-card-detail-green w-full md:w-1/5">
         <Typography
           component="div"
           align="center"
@@ -15,7 +15,7 @@ export default function Business({ title, content }) {
         </Typography>
       </div>
       {/* <Card className="rounded-[0px] bg-base-green w-4/5"> */}
-      <div className="rounded-[0px] w-4/5 background-:">
+      <div className="rounded-[0px] w-full background-: md:w-4/5">
         <Typography component="div" className="text-sm p-4 text-base-gray">
           {content}
         </Typography>
