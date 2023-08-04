c2df974d8c8077b5c26b475df22b0cfc4a8590f2
Hara Ryonosuke <hara_water@volbups.biz>
2023-08-04 21:19:46 +0900

    ヘッダーとユーザコメント部分のレスポンシブ対応修正

--- components/Header.js
+++ components/Header.js
@@ -7,9 +7,10 @@ export default function Header() {
         alt="header"
         className="w-full"
       />
-      <div className="absolute text-white tracking-widest font-header top-[10vw] text-xs 2xl:text-2xl 2xl:top-[20vw] left-[5vw] xl:top-[18vw] xl:text-xl lg:top-[16vw] lg:text-lg md:top-[14vw] md:text-base sm:top-[12vw] sm:text-sm">
+      <div className="absolute text-white tracking-widest font-header top-[10vw] text-sm 2xl:text-3xl 2xl:top-[20vw] left-[5vw] xl:top-[18vw] xl:text-2xl lg:top-[16vw] lg:text-xl md:top-[14vw] md:text-lg sm:top-[12vw] sm:text-base">
         <p>仕事の垣根を超えて</p>
-        <p>つながる大切なエンジニア仲間</p>
+        <p>つながる大切な</p>
+        <p>エンジニア仲間</p>
       </div>
       {/* <div>
       <p>「エンジニアの待遇や働く環境を改善し　みんなでスキル向上を図りたい」</p>
