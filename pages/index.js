c25288fcfd6e4e543316b9679446ebbf8cd1cda2
Hara Ryonosuke <hara_water@volbups.biz>
2023-10-15 19:52:30 +0900

    indexページにContact Usリンクを追加

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Top from '@components/Top'
 import ServiceComponent from '@components/ServiceComponent'
 import UsersVoice from '@components/UsersVoice'
 import News from '@components/News'
+import { LinkCrewH1 } from 'common'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -21,6 +23,9 @@ export default function Home() {
           <ServiceComponent />
           <UsersVoice />
           <News />
+          <Link href="/ContactUs">
+            <LinkCrewH1 title="Contact Us" className="text-center hover:cursor-pointer" />
+          </Link>
         </div>
       </main>
 
