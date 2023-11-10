cd2a143a509bfebd9ea5187087db682edca05010
yui sakai <yu01.24mi77begining@gmail.com>
2023-11-11 03:23:54 +0900

    [fix]next Linkの使用

--- components/common/IconLink.js
+++
@@ -1,7 +0,0 @@
-export default function IconLink({ href, src, alt, imgClassName, linkClassName }) {
-  return (
-    <a href={href} target="_blank" className={linkClassName}>
-      <img src={src} alt={alt} className={imgClassName} />
-    </a>
-  )
-}
