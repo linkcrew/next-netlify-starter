d3a62a86ee16a9525bbf72d5da1193e5c9eb716a
yui sakai <yu01.24mi77begining@gmail.com>
2023-11-13 23:49:57 +0900

    外部リンクを活かす

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -9,9 +9,9 @@ export default function ContactComponent() {
       <LinkCrewDiv title="弊社へのお問い合わせは公式LINEまたは下記のフォームよりお願いいたします" />
       <LinkCrewDiv title="（公式LINEのご登録はこちらから）" className="text-left mt-10"/>
         <Link href="https://lin.ee/YEKMoqp">
-          <div className="hover:cursor-pointer">
-            <IconBrandLine className="w-20 h-20 text-left mb-10"/>
-          </div>
+          <a target="_blank" rel="noopener noreferrer" className="w-20 h-20 mb-10">
+            <IconBrandLine/>
+          </a>
         </Link>
       <LinkCrewDiv title="内容を確認の上、担当よりご返信させていただきますので、お時間いただく可能性がございます。"/>
       <LinkCrewDiv title="また、お問い合わせいただいた内容によってはご返信できない場合がございます。あらかじめご了承ください。"/>
