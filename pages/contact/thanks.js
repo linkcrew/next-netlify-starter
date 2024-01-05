5fe32bff63efa2d488e06f068761b9f956aaac30
yui sakai <yu01.24mi77begining@gmail.com>
2024-01-05 22:23:13 +0900

    googleformのcss調整

--- pages/contact/thanks.js
+++ pages/contact/thanks.js
@@ -1,16 +1,15 @@
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import ContactComponent from '@components/ContactComponent'
-import { LinkCrewH1 } from 'common'
+import ResultGoogleForm from '@components/contact/ResultGoogleForm'
 
-export default function Contact() {
+
+export default function Thanks() {
   return (
     <div aria-label="ページ全体">
       <main className='mx-auto w-4/5'>
-        <Header/>
+      <Header/>
         <div className="flex flex-col gap-y-40" >
-          <LinkCrewH1 title="Contact" />
-          <ContactComponent />
+          <ResultGoogleForm />
         </div>
       </main>
       <Footer />
