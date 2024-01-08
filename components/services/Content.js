2b0efa6c5221e68f88122aad4244b07d064aa11b
yui sakai <yu01.24mi77begining@gmail.com>
2024-01-08 18:18:50 +0900

    サービス遷移

--- components/services/Content.js
+++ components/services/Content.js
@@ -2,7 +2,7 @@ import { LinkCrewH2 } from "common";
 
 export default function Content(props) {
   return (
-    <div className="w-full text-center mt-32" aria-label="各サービスのコンテンツ">
+    <div className="w-full text-center mt-32" aria-label="各サービスのコンテンツ" id={props.title}>
       <LinkCrewH2 title={props.title} className="mb-16"/>
       <div class="w-full text-center" aria-label="各サービスの詳細">
         <img src={props.service_img_path} alt="サービス画像" class="block m-auto"/>
