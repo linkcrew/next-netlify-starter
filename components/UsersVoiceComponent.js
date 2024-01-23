337995775127d09eaccc1ed279ebcc7774838bee
Hara Ryonosuke <hara_water@volbups.biz>
2024-01-23 21:45:28 +0900

    利用者の声の添付画像の枠線の修正漏れ

--- components/UsersVoiceComponent.js
+++ components/UsersVoiceComponent.js
@@ -50,7 +50,7 @@ export default function UsersVoiceComponent() {
                   <LinkCrewDiv title={value.name} className="text-right" />
                 </div>
                 <div className="w-[0%] md:basis-1/5 my-auto">
-                  <img src={value.src} className="bg-none border-4 border-gray-300"/>
+                  <img src={value.src} className="bg-none border-0 md:placeholder:border-4 border-gray-300"/>
                 </div>
               </div>
             : <div className="flex flex-col gap-x-5 md:flex-row">
